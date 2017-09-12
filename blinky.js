e1c4de5f23021d978fe55f5b21e7d5dd29f0dcbb
Sean Pollock <seanpollock3344@gmail.com>
2017-09-12 09:34:08 -0400

    messing with blinking led's

--- blinky.js
+++ blinky.js
@@ -5,10 +5,11 @@ var tessel = require('tessel');
 
 var led1 = tessel.led[0].output(1);
 var led2 = tessel.led[1].output(0);
+var led3 = tessel.led[2].output(0)
 
 setInterval(function() {
-	console.log("yess I am blinking!")
 	//toggle the led states
 	led1.toggle();
 	led2.toggle();
+	led3.toggle()
 }, 1000);
