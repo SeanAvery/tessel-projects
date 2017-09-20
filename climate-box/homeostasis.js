8eaa1b4d8cb3ac2e36fcfca15f94a041e5de07e7
Sean Pollock <seanpollock3344@gmail.com>
2017-09-20 11:34:46 -0700

    connected to climate and relay module

---
+++ climate-box/homeostasis.js
@@ -0,0 +1,21 @@
+var tessel = require('tessel')
+var climatelib = require('climate-si7020')
+var relaylib = require('relay-mono')
+var climate = climatelib.use(tessel.port['D'])
+var relay = relaylib.use(tessel.port['C'])
+
+climate.on('ready', function () {
+  console.log('### connected to climate module')
+  // setImmediate(function loop () {
+  //   climate.readTemperature('f', function (err, temp) {
+  //     if (err) console.log('### ERROR in readTemperature', err)
+  //     console.log('### Degrees', temp.toFixed(5))
+  //     if (temp >= 80)
+  //     setTimeout(loop, 1000)
+  //   })
+  // })
+})
+
+relay.on('ready', function () {
+  console.log('### connected to relay module')
+})
