a9ad332c10a7dca3034508ac36c11e72ebac42a2
Sean Pollock <seanpollock3344@gmail.com>
2017-09-20 11:31:36 -0700

    rebuilding climate flow

--- climate/climate.js
+++ climate/climate.js
@@ -1,32 +1,16 @@
-// Any copyright is dedicated to the Public Domain.
-// http://creativecommons.org/publicdomain/zero/1.0/
+var tessel = require('tessel')
+var climatelib = require('climate-si7020')
+var relaylib = require('relay-mono')
+var climate = climatelib.use(tessel.port['D'])
+var relay = relaylib.use(tessel.port['C'])
 
-/*********************************************
-This basic climate example logs a stream
-of temperature and humidity to the console.
-*********************************************/
-
-var tessel = require('tessel');
-// if you're using a si7020 replace this lib with climate-si7020
-var climatelib = require('climate-si7020');
-
-var climate = climatelib.use(tessel.port['A']);
-
-
-climate.on('ready', function () {
-  console.log('Connected to si7005');
-
-  // Loop forever
+climate.on('ready', function() {
+  console.log('### connected to climate module')
   setImmediate(function loop () {
     climate.readTemperature('f', function (err, temp) {
-      climate.readHumidity(function (err, humid) {
-        console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH');
-        setTimeout(loop, 300);
-      });
-    });
-  });
-});
-
-climate.on('error', function(err) {
-  console.log('error connecting module', err);
-});
+      if (err) console.log('### ERROR in readTemperature', err)
+      console.log('### Degrees', temp.toFixed(5))
+      setTimeout(loop, 1000)
+    })
+  })
+})
